382e5f2f49fed53e6162dd41d9e4c8bb8c40ff08
chenwei <1015033746@qq.com>
2017-01-10 23:14:07 +0800

    add itertools

--- python_knowledge/itertools_knowledge.py
+++ python_knowledge/itertools_knowledge.py
@@ -133,7 +133,7 @@ print (list(c))
 
 # 截取第一个不满足条件的序列到结尾
 """
-def (predicate, iterable):
+def dropwhile(predicate, iterable):
 	iterbale = iter(iterable)
 
 	for x in iterable:
@@ -162,3 +162,101 @@ def filterfalse(predicate, iterable):
 from itertools import filterfalse
 f = filterfalse(None, ["aa", "", 0, "v", 0.0])
 print (list(f))
+
+# 获取知道调用predicate(x)为False之前的数据(和dropwhile类似)
+#如果第一条就不满足的话，就返回空迭代器
+"""
+def takewhile(predicate, iterable):
+	for x in iterable:
+		if predicate(x):
+			yield x
+		else:
+			break
+"""
+from itertools import takewhile
+t = takewhile(lambda x : x > 5, [1, 2, 3, 4, 5])
+print (list(t))
+
+#组合 group
+"""
+将key函数作用于原循环器的各个元素。根据key函数结果，将拥有相同函数结果的元素分到一个新的循环器。每个新的循环器以函数返回结果为标签。
+这就好像一群人的身高作为循环器。
+我们可以使 用这样一个key函数: 如果身高大于180，返回"tall"；如果身高底于160，返回"short";中间的返回"middle"。最终，所有身高将分为三个循环器， 即"tall", "short", "middle"。
+"""
+
+from itertools import groupby
+
+def high_class(h):
+	if h> 180:
+		return "tall"
+	elif h < 160:
+		return "short"
+	else:
+		return "middle"
+
+friends = [191, 158, 159, 165, 170, 177, 181, 182, 190]
+
+friends = sorted(friends, key=high_class)
+
+for m,n in groupby(friends,key=high_class):
+	print (m, list(n))
+#注意，groupby的功能类似于UNIX中的uniq命令。
+#分组之前需要使用sorted()对原循环器的元素，根据key函数进行排序，让同组元素先在位置上靠拢。
+
+# islice() 命名切片迭代器
+"""
+def islice(iterable, *args):
+	s = slice(*args) #生成切片对象
+	it = iter(range(s.start or 0, s.stop or sys.maxsize, s.step or 1))
+	
+	try:
+		nexti = next(it)
+	except StopIteration:
+		return
+	for i, element in enumerate(iterable):
+		if i == nexti:
+			yield element
+			nexti = next(it)
+"""
+from itertools import islice
+islice("ABCDEFG", 2) #  start = 0, stop = 2, step = 1
+islice("ABCDEFG", 2, 4) # start = 2, stop = 4, step = 1
+islice("ABCDEFG", 2, None) # start = 2, stop = sys.maxsize, step = 1
+islice("ABCDEFG", 0, None, 2) # start=2, stop=sys.maxsize, step = 2
+
+"""
+def tee(iterable, n = 2):
+	it = iter(iterable)
+	deques = [collections.deque() for i in range(n)]
+	def gen(mydeque):
+		while True:
+			if not mydeque:  # when the local deque is empty
+				try:
+					newval = next(it)
+				except StopIteration:
+					return 
+				for d in deques:
+					d.append(newval)
+			yield mydeque.popleft()
+	return tuple(gen(d) for d in deques)
+"""
+
+"""从iterable创建n个独立的迭代器，创建的迭代器以n元组的形式返回，n的默认值为2，
+此函数适用于任何可迭代的对象，但是，为了克隆原始迭代器，生成的项会被缓存，
+并在所有新创建的迭代器中使用，
+一定要注意，不要在调用tee()之后使用原始迭代器iterable，否则缓存机制可能无法正确工作
+"""
+from itertools import tee
+t = tee([1, 2, 3])
+print (list(t))
+
+
+"""
+操作zip 函数时，多余的话，会被截取掉
+zip_longest() 则会补充fillvalue, 默认值为None
+"""
+from itertools import zip_longest
+z = zip_longest('abc', 'bacd', fillvalue='-')
+print(list(z)) #->[('a', 'b'), ('b', 'a'), ('c', 'c'), ('-', 'd')]
+z = zip("abcdd", "edf")
+print (list(z))
